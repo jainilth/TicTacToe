e9a6f6144ce89ecbc2f96dc9e51db65f3a59f9b8
@jainil@ <jainilthummar2005@gmail.com>
2024-08-15 12:30:29 +0530

    15-8-24

--- index.js
+++ index.js
@@ -1,72 +1,63 @@
-person1 = true
-a = []
-count = 0
-aRed = []
-aBlue = []
+let person1 = true;
+let a = [];
+let count = 0;
+let aRed = [];
+let aBlue = [];
+
 const checkWin = () => {
-    console.log(a)
-    const targetValues = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-    for (i = 0; i < 8; i++) {
-        // if(targetValues[i][0]==aRed[0]&&targetValues[i][1]==aRed[1]&&targetValues[i][2]==aRed[2]) {
-        //     alert('person 1 win')
-        //     window.location.reload();
-        // }
-        // if(targetValues[i][0]==aBlue[0]&&targetValues[i][1]==aBlue[1]&&targetValues[i][2]==aBlue[2]) {
-        //     alert('person 2 win')
-        //     window.location.reload();
-        // }
-        const allMatch1 = targetValues[i].every((value) => aRed.includes(value));
+    const targetValues = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    for (let i = 0; i < 8; i++) {
+        const allMatch1 = targetValues[i].every(value => aRed.includes(value));
         if (allMatch1) {
-            // if(aRed.le)
-            alert('person 1 win')
+            alert('Person 1 wins!');
             window.location.reload();
             return;
-        } 
-        const allMatch2 = targetValues[i].every((value) => aBlue.includes(value));
+        }
+        const allMatch2 = targetValues[i].every(value => aBlue.includes(value));
         if (allMatch2) {
-            // if(aRed.le)
-            alert('person 2 win')
+            alert('Person 2 wins!');
             window.location.reload();
             return;
-        } 
+        }
+    }
+    if (count === 8) {
+        alert('It\'s a draw!');
+        window.location.reload();
     }
 }
+
 const changeColor = (id, n) => {
     const box = document.getElementById(id);
     if (!a[n]) {
         if (person1) {
-            box.style.backgroundColor = "red";
-            person1 = false
-            aRed.push(n)
-            aRed.sort((a, b) => {
-                return a - b;
-            })
-            console.log(aRed)
-            count++
-            if (count > 4) {
-                checkWin()
+            box.classList.add('x-mark');
+            person1 = false;
+            aRed.push(n);
+            aRed.sort((a, b) => a - b);
+            if (count >= 4) {
+                checkWin();
             }
-            a[n] = 'X'
-            return;
-        }
-        else {
-            box.style.backgroundColor = "blue";
-            person1 = true
-            aBlue.push(n)
-            aRed.sort((a, b) => {
-                return a - b;
-            })
-            console.log(aBlue)
-            count++
-            if (count > 4) {
-                checkWin()
+            a[n] = 'X';
+        } else {
+            box.classList.add('o-mark');
+            person1 = true;
+            aBlue.push(n);
+            aBlue.sort((a, b) => a - b);
+            if (count >= 4) {
+                checkWin();
             }
-            a[n] = 'O'
-            return;
+            a[n] = 'O';
         }
+        count++;
+    } else {
+        alert("Move not possible!");
     }
-    else {
-        alert("move not possible!")
-    }
-
 }
+
+
+
